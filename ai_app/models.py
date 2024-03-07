e9654a72568042ab520f74d7f3481e1ed777b8d3
Oktawia-K <mati13553@gmail.com>
2024-03-07 17:34:24 +0100

    project init

---
+++ ai_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+class ImageElements(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="mediaphoto", blank=True, null=True)
